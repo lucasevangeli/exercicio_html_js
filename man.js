04056251b82861d7eb1ec27540d2dcef985d4c4d
Lucas Evan <lucasevan478@gmail.com>
2023-12-06 12:30:51 -0300

    edicao

--- man.js
+++ man.js
@@ -1,7 +1,7 @@
 
 document.getElementById('form').addEventListener('submit', function(event) {
     
-    event.preventDefault();
+     event.preventDefault();
 
     
     let campoA = document.getElementById('campoA').value;
