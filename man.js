239e2fee9b16b5bef4c72c5f8dcfa3be2fe34871
Lucas Evan <lucasevan478@gmail.com>
2023-12-06 11:52:00 -0300

    adição da validação

--- man.js
+++ man.js
@@ -1,13 +1,18 @@
 
-
-    let campoA = document.getElementById('campoA')
-    let campoB = document.getElementById('campoB')
+document.getElementById('meuFormulario').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
 
     
-    if (campoB  > campoA) {
+    let campoA = document.getElementById('campoA').value;
+    let campoB = document.getElementById('campoB').value;
+
+    
+    if (parseInt(campoB) > parseInt(campoA)) {
         
         alert('Formulário válido! Número B é maior que o número A.');
     } else {
         
         alert('Erro! Número B deve ser maior que o número A.');
     }
+});
