55c71fc214a8445f1e634371e8ee47c81f7814d6
Lucas Evan <lucasevan478@gmail.com>
2023-12-06 08:57:47 -0300

    validação do form

---
+++ man.js
@@ -0,0 +1,13 @@
+
+
+    let campoA = document.getElementById('campoA')
+    let campoB = document.getElementById('campoB')
+
+    
+    if (campoB  > campoA) {
+        
+        alert('Formulário válido! Número B é maior que o número A.');
+    } else {
+        
+        alert('Erro! Número B deve ser maior que o número A.');
+    }
